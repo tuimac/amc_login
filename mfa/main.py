3f010a44fc7eab6bf6c76eac999aede3b6127d18
tuimac <tuimac.devadm01@gmail.com>
2020-06-28 15:08:26 +0900

    create new

---
+++ mfa/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+import sys
+
+LINUXPATH = ""
+WINPATH = ""
+
+def main():
+    #driver = webdriver.Chrome(LINUXPATH)
+    print(sys.platform)
+
+if __name__ == "__main__":
+    main()
